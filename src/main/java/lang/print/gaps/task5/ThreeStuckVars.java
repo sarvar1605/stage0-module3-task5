a1100c647bcbe74e22319e3f0c7c584f1bfeedf4
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-10-01 21:58:39 +0500

    Update ThreeStuckVars.java

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -3,8 +3,6 @@ package lang.print.gaps.task5;
 public class ThreeStuckVars {
     public static void main(String[] args) {
         int first=1, second=10, third=100;
-        System.out.print(first);
-        System.out.print(second);
-        System.out.print(third);
+        System.out.println(first+""+second+""+third);
     }
 }
